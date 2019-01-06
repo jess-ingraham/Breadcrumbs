128dbb9371ab0808afb4499b5dc39c2ba2d04d06
jess-ingraham <jli232@nyu.edu>
2019-01-05 23:38:50 -0500

    home page and sign in page styled

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname , '..', 'public', 'html' ,'index.html'));
+});
+
+router.get('/home', function(req, res, next){
+	res.redirect(301, '/')
+});
+
+router.get('/signup', function(req, res, next){
+	res.sendFile(path.join(__dirname , '..', 'public', 'html' ,'signup.html'));
+
+});
+
+router.get('/login', function(req, res, next){
+	res.sendFile(path.join(__dirname , '..', 'public', 'html' ,'login.html'));
 });
 
 module.exports = router;
